b7dd563cd2a01ec71c266c7d53fb980785e092b8
Shuhei Iitsuka <tushuhei@google.com>
2025-04-03 19:08:16 +0900

    Install Storybook (#59)

---
+++ src/stories/pv-button.stories.ts
@@ -0,0 +1,48 @@
+import '../pv-button';
+
+import type {Meta, StoryObj} from '@storybook/web-components';
+import {html} from 'lit';
+
+const meta = {
+  title: 'Components/pv-button',
+  component: 'pv-button',
+  tags: ['autodocs'],
+  argTypes: {
+    label: {control: 'text'},
+    active: {control: 'boolean'},
+    rounded: {control: 'boolean'},
+  },
+  render: args => {
+    return html`<pv-button
+      label=${args.label}
+      ?active=${args.active}
+      ?rounded=${args.rounded}
+    ></pv-button>`;
+  },
+} satisfies Meta;
+
+export default meta;
+
+export const Default = {
+  args: {
+    label: 'Button',
+    active: false,
+    rounded: false,
+  },
+} satisfies StoryObj;
+
+export const Active = {
+  args: {
+    label: 'Active',
+    active: true,
+    rounded: false,
+  },
+} satisfies StoryObj;
+
+export const Rounded = {
+  args: {
+    label: 'Rounded',
+    active: false,
+    rounded: true,
+  },
+} satisfies StoryObj;
